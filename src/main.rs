eee118c9f7ebd2875c5f668af3399b80e0fe3d6c
Felipe Sere <felipesere@gmail.com>
2020-03-16 19:58:55 +0100

    More straight forward type for YAML steps

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,62 @@
 use core::fmt;
-use serde::de::{Unexpected, Visitor};
-use serde::{de, Deserialize, Deserializer};
+use serde::de::Visitor;
+use serde::{Deserialize, Deserializer};
 use std::fs::File;
 
 #[derive(Debug, Eq, PartialEq)]
-enum OneOrMany {
-    One(String),
-    Many(Vec<String>),
+struct Steps {
+    values: Vec<String>,
 }
 
-impl<'de> Deserialize<'de> for OneOrMany {
+impl<'de> Deserialize<'de> for Steps {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
     {
-        one_or_many(deserializer)
-    }
-}
+        struct OneOrManyVisitor {}
 
-fn one_or_many<'de, D>(deserializer: D) -> Result<OneOrMany, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    struct OneOrManyVisitor {}
+        impl<'de> Visitor<'de> for OneOrManyVisitor {
+            type Value = Steps;
 
-    impl<'de> Visitor<'de> for OneOrManyVisitor {
-        type Value = OneOrMany;
-
-        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-            formatter.write_str("either \"auto\" or a port number")
-        }
-
-        fn visit_str<E>(self, value: &str) -> Result<Self::Value, E> {
-            Ok(OneOrMany::One(value.to_owned()))
-        }
-
-        fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-        where
-            A: serde::de::SeqAccess<'de>,
-        {
-            let mut result = Vec::new();
-
-            while let Ok(Some(val)) = seq.next_element::<String>() {
-                result.push(val)
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("either \"auto\" or a port number")
             }
 
-            Ok(OneOrMany::Many(result))
-        }
-    }
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E> {
+                Ok(Steps {
+                    values: vec![value.to_owned()],
+                })
+            }
 
-    deserializer.deserialize_any(OneOrManyVisitor {})
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::SeqAccess<'de>,
+            {
+                let mut values = Vec::new();
+
+                while let Ok(Some(val)) = seq.next_element::<String>() {
+                    values.push(val)
+                }
+
+                Ok(Steps { values })
+            }
+        }
+
+        deserializer.deserialize_any(OneOrManyVisitor {})
+    }
 }
 
 #[derive(Debug, PartialEq, Deserialize)]
 struct FolderConfig {
-    fmt: OneOrMany,
-    build: OneOrMany,
-    test: OneOrMany,
-    check: OneOrMany,
+    fmt: Steps,
+    build: Steps,
+    test: Steps,
+    check: Steps,
 }
 
 fn main() {
     let file = File::open("./fern.yaml").unwrap();
 
     let config: FolderConfig = serde_yaml::from_reader(file).unwrap();
-    println!("Hello, world!");
-    println!("{:?}", config);
+    println!("{:#?}", config);
 }
