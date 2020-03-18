d65742a6336932d38493c60f32898e274ef93ad4
Felipe Sere <felipesere@gmail.com>
2020-03-18 17:20:40 +0100

    Extract steps into own module

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,13 @@
-use core::fmt;
-use serde::de::Visitor;
-use serde::{Deserialize, Deserializer};
+use git_version::git_version;
+use serde::Deserialize;
 use std::fs::File;
 
-use git_version::git_version;
+use steps::Steps;
+
 const GIT_VERSION: &str = git_version!();
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-#[derive(Debug, Eq, PartialEq)]
-struct Steps {
-    pub values: Vec<String>,
-}
-
-impl Default for Steps {
-    fn default() -> Self {
-        Steps { values: Vec::new() }
-    }
-}
-
-impl<'de> Deserialize<'de> for Steps {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        struct OneOrManyVisitor {}
-
-        impl<'de> Visitor<'de> for OneOrManyVisitor {
-            type Value = Steps;
-
-            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-                formatter.write_str("either single string or sequence of strings")
-            }
-
-            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E> {
-                Ok(Steps {
-                    values: vec![value.to_owned()],
-                })
-            }
-
-            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-            where
-                A: serde::de::SeqAccess<'de>,
-            {
-                let mut values = Vec::new();
-
-                while let Ok(Some(val)) = seq.next_element::<String>() {
-                    values.push(val)
-                }
-
-                Ok(Steps { values })
-            }
-        }
-
-        deserializer.deserialize_any(OneOrManyVisitor {})
-    }
-}
+mod steps;
 
 #[derive(Eq, PartialEq)]
 enum Depth {
---
+++ src/steps.rs
@@ -0,0 +1,52 @@
+use core::fmt;
+use serde::de::Visitor;
+use serde::{Deserialize, Deserializer};
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Steps {
+    pub values: Vec<String>,
+}
+
+impl Default for Steps {
+    fn default() -> Self {
+        Steps { values: Vec::new() }
+    }
+}
+
+impl<'de> Deserialize<'de> for Steps {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct OneOrManyVisitor {}
+
+        impl<'de> Visitor<'de> for OneOrManyVisitor {
+            type Value = Steps;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("either single string or sequence of strings")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E> {
+                Ok(Steps {
+                    values: vec![value.to_owned()],
+                })
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::SeqAccess<'de>,
+            {
+                let mut values = Vec::new();
+
+                while let Ok(Some(val)) = seq.next_element::<String>() {
+                    values.push(val)
+                }
+
+                Ok(Steps { values })
+            }
+        }
+
+        deserializer.deserialize_any(OneOrManyVisitor {})
+    }
+}
